aa5df48255e7d9deb8ad3585e4817596e88bf222
Gogoncalves18 <gogoncalves@gmail.com>
2023-02-13 22:30:52 -0300

    Mais exercicios de tupla

---
+++ Python_EstudoNauta/A16_Ex_074 sorteio de uma tupla.py
@@ -0,0 +1,6 @@
+#randomizar 5 numero e colocar em uma tupla e depuis pegar o menor e maior valor dentro da tupla
+import random
+vlr_sort=(random.randint(1,9),random.randint(1,9),random.randint(1,9),random.randint(1,9),random.randint(1,9))
+print(vlr_sort)
+print(min(vlr_sort))
+print(max(vlr_sort))
---
+++ Python_EstudoNauta/A16_Ex_075  valores da tupla.py
@@ -0,0 +1,15 @@
+#Digitar 4 numeros pra entrar em uma tupla e depois analisar quantas vezes o 9 aparece,
+#qual a posicao que o 3 aparece
+#quais sao os numeros pares da tupla
+#OBS>: nao fiz tratamento de expection
+n=(int(input('Digite um numero: ')), int(input('Digite um numero: ')), int(input('Digite um numero: ')),
+int(input('Digite um numero: ')))
+print(f'O numero 9 apareceu {n.count(9)}x!')
+print(f'O valor 3 apareceu na posicao {n.index(3)}')
+print(f'Os valores pares digitados foram: ', end='')
+for i in n:
+    if i%2==0:
+        print(i, end=' - ')
+    else:
+        pass
+print('')
