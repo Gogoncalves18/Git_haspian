fdbdc384276bdb559c5171b6052868accb0335f2
Gogoncalves18 <gogoncalves@gmail.com>
2023-02-21 21:55:33 -0300

    Ordenacao de listas que estao dentro de outra lista

--- Python_EstudoNauta/teste.py
+++ Python_EstudoNauta/teste.py
@@ -1,26 +1,3 @@
-lst_p=['gu', 40, 'val', 40, 'sogra', 60]
-lst_p_estr=[]
-lst_y_old=[]
-lst_y_young=[]
-print(lst_p)
-pos=0
-idade_m=0
-for v in lst_p:
-    if type(v) is str:
-        lst_p_estr.append(lst_p[pos:(pos+2)])
-        pos+=1
-    else:
-        if v == idade_m:
-            lst_y_old.append(lst_p[(pos-1):(pos+1)])
-            idade_m=v
-        elif v < idade_m:
-            continue
-        elif v > idade_m:
-            del lst_y_old[:]
-            lst_y_old.append(lst_p[(pos-1):(pos+1)]) 
-            idade_m=v
-        pos+=1
-        
-print(lst_p_estr)
-print(lst_y_old)
+lst_p=[[8,2,6],[3,1,9]]
+print(sorted(lst_p[0]))
   
