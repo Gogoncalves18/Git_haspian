d6d175c3317fd2c099b756ff863be204a3a02e18
Gogoncalves18 <gogoncalves@gmail.com>
2023-02-24 18:15:34 -0300

    Fatiamento e randomizacao de numeros para Mega Sena

---
+++ Python_EstudoNauta/A17_Ex_088 Sorteio de jogos da MegaSena.py
@@ -0,0 +1,12 @@
+#Programa deve pegir o numero de jogo para o usuarios e sortear 6 numeros
+#usando listas
+import random 
+print(f'{"-_-"*20} SORTEIO PARA MEGA {"-_-"*20}')
+sorteio_temp=[] #Lista temporaria para receber e passar vlr e limpar
+sorteio_final=[]
+qtd_jogos=int(input('\nQuantos jogos vc deseja: '))
+for j in range(0,qtd_jogos): #N jogos que o usuario quer que calcule
+    sorteio_temp.append(random.sample(range(1,60),6)) #TIPO DE RANDOM QUE RETORNA 6 VALORES QUE NAO REPETEM
+    sorteio_final.append(sorteio_temp[0]) #Fatiamento da copiada para lista final
+    sorteio_temp.clear() #limpeza da lista temporaria
+    print(f'\nSoteio N_{j+1} => {sorted(sorteio_final[j])}')
--- Python_EstudoNauta/teste.py
+++ Python_EstudoNauta/teste.py
@@ -1,3 +1,6 @@
-lst_p=[[8,2,6],[3,1,9]]
-print(sorted(lst_p[0]))
-  
+import random
+lst_p=[8,2,3,1,9]
+lst_2=[]
+for c in range(0,2):
+    lst_2.append(random.sample(range(1,60),6))
+print(lst_2)
