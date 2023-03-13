d63cc8e5ec4ba3a3c6dfc7e45c7de62a70593af0
Gogoncalves18 <gogoncalves@gmail.com>
2023-03-13 12:51:44 -0300

    Validacao de dados digitados para dentro da funcao. Ou uso vlrs pre definidos na funcao ou uso o valor digitado

---
+++ Python_EstudoNauta/A21_Ex_103 Ficha de gols por jogador em um funcao.py
@@ -0,0 +1,21 @@
+'''Crie um programa que tenha uma funcao chamada ficha(), que receba
+dois parametros opcionais: o Nome e o numero de gols e por fim apresente
+a ficha do jogador. A entrada de dados pode ser numerica ou string.
+'''
+
+def ficha(nome='desconhecido', gols='0'): #Defini parametro fixo caso não receba nada
+    return (f'\nO jogador {nome}, fez {gols} gols!')
+
+
+#PROGRAMA PRINCIPAL:
+
+jog = str(input(f'\nDigite nome do jogador: ')) #Pego o nome do jogador, se foi digitado
+gol = str(input(f'Digite o numero de gols dele: ')) #Pego os gols, se foi digitado
+if jog.strip() == '' and gol.isnumeric(): #Dou um strip para tirar qq espaco e se ele for vazio para o nome jogador e há numero em gols executo o print abaixo, so com gols
+    print(ficha(gols=gol))
+elif jog.strip() == '' and gol == '': #Se o nome do jogador e gol vier vazio, passo para funcao a execução dos parâmetros pre configurados
+    print(ficha())
+elif len(jog) > 1 and gol == '': #Se recebo algo maior que 1 em nome do jogador, é porque escreveram algo e valido se há gol
+    print(ficha(nome=jog))
+elif len(jog) > 1 and gol.isnumeric(): #Aqui valido se tem nome escrito e gol escrito
+    print(ficha(nome=jog, gols=gol))
