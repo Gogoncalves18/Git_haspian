79bdcb91e3a242da6c5ec8e3f5fa8cf0f257436b
Gogoncalves18 <gogoncalves@gmail.com>
2023-03-14 08:12:08 -0300

    Funcao para validar se a entrada de dado é um INT.

---
+++ Python_EstudoNauta/A21_Ex_104 Validacao numerica dentro de uma funcao.py
@@ -0,0 +1,19 @@
+'''Crie um programa que leia uma entrada de informação e aceite apenas numero inteiro.
+Qualquer outra coisa como espaço, letras e valor vazio deve ser avisado ao usuário.
+'''
+
+def validar_num(info): #Recebo uma informacao na funcao
+    while True: #Abro um looping para ficar validando a info
+        try: #Abro o except tentando verificar se a info é um inteiro
+            if int(info):
+                print(f'\n\033[1;33;45m {info} \033[m') #aqui eu poderia construir um retorno
+        except ValueError: #Se a info nao for um inteiro ele subira um tracing back de ValueError
+            info = input(f'\n\033[1;37;43mVc precisa digitar um numero inteiro:\033[m ') #Peco uma nova entrada
+            continue #Continuo na validacao
+        else:
+            break #Se der tudo certo, encerro o looping
+
+
+#PROGRAMA PRINCIPAL
+n = input(f'\nDigite um numero: ')
+validar_num(n)
