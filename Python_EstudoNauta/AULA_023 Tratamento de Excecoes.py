e3813dd06f144ea20d43ce5a8147312fd2c0fbaf
Gogoncalves18 <gogoncalves@gmail.com>
2023-03-25 19:29:54 -0300

    Aula de tratamento de exceções

---
+++ Python_EstudoNauta/AULA_023 Tratamento de Excecoes.py
@@ -0,0 +1,32 @@
+a = 10
+b = 2
+
+try:
+    c = a/b
+except ValueError:
+    print('LINHA 7 - Houve erro')
+else: #Esta clausula é opcional e será executada somente se não ocorrer exceções no programa
+    print(f'LINHA 9 - {c}')
+finally: #Esta clausula é opcional e será executada com erro ou não
+    print(f'\nLINHA 11 - Obrigado, programa encerrado')
+
+a = 10
+b = 'tt'
+
+try:
+    c = a/b
+except Exception as erro: #aqui o exception é uma classe que grava o tipo de erro na variavel
+    #print(f'\nLINHA 19 - Anotação: {erro.__annotations__}')
+    #print(f'\nLINHA 20 - Causa: {erro.__cause__}')
+    print(f'\nLINHA 21 - Classe: {erro.__class__}') #Apresenta a classe do objeto que devolveu o erro
+    #print(f'\nLINHA 22 - Contexto: {erro.__context__}')
+    #print(f'\nLINHA 23 - Dict: {erro.__dict__}')
+    #print(f'\nLINHA 24 - Delattr: {erro.__delattr__}')
+    #print(f'\nLINHA 25 - Doc: {erro.__doc__}') #Traz uma explicação mais explicita e didatica do erro
+    #print(f'\nLINHA 26 - Repr: {erro.__repr__}')
+    #print(f'\nLINHA 27 - Sizeof: {erro.__sizeof__}')
+    #print(f'\nLINHA X - Anotação: {erro}')
+else:
+    print(f'LINHA 29 - {c}')
+finally:
+    print(f'\nLINHA 31 - Obrigado, programa encerrado')    
