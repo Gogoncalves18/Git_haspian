9b63178a5a74dd79e183e39d01bd6252220b99db
Gogoncalves18 <gogoncalves@gmail.com>
2023-03-25 19:28:47 -0300

    Exercicio de exceções

---
+++ Python_EstudoNauta/A23_Ex_113 Tratamento de excecoes na entrada de num.py
@@ -0,0 +1,30 @@
+'''Programa para validar exceções da entrada de um numero inteiro e um real'''
+from Modulos_Uteis import cores
+
+sair = False
+while sair is False:
+    try:
+        num_int = int(input('\nDigite um numero inteiro: '))
+    #except Exception as erro:
+        #print(f'O tipo de erro foi {erro.__class__}')
+    except ValueError:
+        cores.titulo('Você não digitou um numero inteiro', 1)
+    except KeyboardInterrupt:
+        cores.titulo('Usuario abortou o programa.', 3)
+        break
+    else:
+        print(num_int)
+        break
+while sair is False:
+    try:
+        num_fl = int(input('\nDigite um numero real: '))
+    #except Exception as erro:
+        #print(f'O tipo de erro foi {erro.__class__}')
+    except ValueError:
+        cores.titulo('Você não digitou um numero inteiro', 1)
+    except KeyboardInterrupt:
+        cores.titulo('Usuario abortou o programa.', 3)
+        break
+    else:
+        print(num_fl)
+        break
