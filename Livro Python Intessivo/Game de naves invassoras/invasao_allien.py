2edd4e3263576a1781a4ec67930eab14fb40d02a
Gogoncalves18 <gogoncalves@gmail.com>
2023-04-28 22:31:14 -0300

    Criação do grupo de aliens e nova funcao para criar a frota de aliens assim como ajuste no update screen

--- Livro Python Intessivo/Game de naves invassoras/invasao_allien.py
+++ Livro Python Intessivo/Game de naves invassoras/invasao_allien.py
@@ -12,10 +12,13 @@ pygame.display.set_caption("Invasão Alien")
 ship = Ship(ai_settings, screen) #cria uma espaçonave antes do while para não criar uma espaçonave a cada laço
 bullets = Group() #Cria um grupo ao qual serão armazenado os projeteis, como se fosse uma instancia
 bg_color = ai_settings.bg_color #Cor para tela fundo
+#alien = Alien(ai_settings, screen)
+aliens = Group() #Grupo da frota de aliens
+gf.create_fleet(ai_settings, screen, aliens)
 while True:
     gf.check_events(ai_settings, screen, ship, bullets) # Função para pegar os eventos do teclado, aqui inicia os modulos de objetos de nave e projeteis
     ship.update()
     gf.update_bullets(bullets) #Chama a funcao para atualizar os projeteis em game_functions
-    gf.update_screen(ai_settings, bg_color, screen, ship, bullets) #aqui atualizo a tela com os objetos
+    gf.update_screen(ai_settings, bg_color, screen, ship, aliens, bullets) #aqui atualizo a tela com os objetos
     run_game() #puxa o metodo que inicializa o jogo e entra no laço principal
 
