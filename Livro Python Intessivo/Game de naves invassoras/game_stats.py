515414bd406f6d249c3cba31f4fbb95dc9b2de4f
Gogoncalves18 <gogoncalves@gmail.com>
2023-05-21 09:07:52 -0300

    Adicionado atualizacao de pontuacao maxima

--- Livro Python Intessivo/Game de naves invassoras/game_stats.py
+++ Livro Python Intessivo/Game de naves invassoras/game_stats.py
@@ -3,10 +3,12 @@ class GameStats():
     def __init__(self, ai_settings):
         """Inicializa os dados estatisticos"""
         self.ai_settings = ai_settings
+        self.high_score = 0 #Vlr pontuacao maxima, nunca deve ser reiniciada
         self.reset_stats()
         self.game_active = False #Flag para encerrar o jogo sempre que a vida acabar
 
     def reset_stats(self):
         """Inicializa os dados estatisticos que podem mudar ao longo do jogo"""
         self.ships_left = self.ai_settings.ship_limit #numero de vidas no jogo
+        self.score = 0
     
