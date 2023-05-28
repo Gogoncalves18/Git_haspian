07a91c98ba3da925e348599998fba966ec6a01c0
Gogoncalves18 <gogoncalves@gmail.com>
2023-05-28 08:02:59 -0300

    Atualizado parametro para atender no funcao de placar, mexemos em update_aliens dando novo parametro SB para ele

--- Livro Python Intessivo/Game de naves invassoras/invasao_allien.py
+++ Livro Python Intessivo/Game de naves invassoras/invasao_allien.py
@@ -22,11 +22,11 @@ bg_color = ai_settings.bg_color #Cor para tela fundo
 aliens = Group() #Grupo da frota de aliens
 gf.create_fleet(ai_settings, screen, ship, aliens)
 while True:
-    gf.check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets) # Função para pegar os eventos do teclado, aqui inicia os modulos de objetos de nave e projeteis
+    gf.check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets) # Função para pegar os eventos do teclado, aqui inicia os modulos de objetos de nave e projeteis
     if stats.game_active:
         ship.update()
         gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets) #Chama a funcao para atualizar os projeteis em game_functions
-        gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets) #Chama o metodo para atualizar a posição dos aliens dentro do grupo aliens
+        gf.update_aliens(ai_settings, stats, screen, sb, ship, aliens, bullets) #Chama o metodo para atualizar a posição dos aliens dentro do grupo aliens
         #E controlar o numero de vidas e reconstruir a frota alien quando ela atingir a nave
     gf.update_screen(ai_settings, bg_color, screen, stats, sb, ship, aliens, bullets, play_button) #aqui atualizo a tela com os objetos
     run_game() #puxa o metodo que inicializa o jogo e entra no laço principal
