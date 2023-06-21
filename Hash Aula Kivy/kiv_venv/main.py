45a1b7f6bc29a7b62d346209027a6bf03293453d
Gogoncalves18 <gogoncalves@gmail.com>
2023-06-20 22:41:19 -0300

    Aula 2 - Adicao boxlayout, label e widget dentro do outro

--- Hash Aula Kivy/kiv_venv/main.py
+++ Hash Aula Kivy/kiv_venv/main.py
@@ -1,8 +1,26 @@
+#Aula: HashLDash - https://www.youtube.com/watch?v=xdxHJIfgG40&list=PLsMpSZTgkF5AV1FmALMgW8W-TvrfR3nrs&index=2
 from kivy.app import App #Importo a classe do aplicativo
 from kivy.uix.button import Button #Da classe uix de interfaces eu importo um botao
+from kivy.uix.boxlayout import BoxLayout #Constroi telas que empilha widget
+from kivy.uix.label import Label
 
 class Test(App): #Herdo da classe app para minha classe teste do app que farei
     def build(self): #chamo a funcao que controi o app
-        return Button(text='Ola Mundo') #retorno dela a classe botao para construir o botao e posso inputar texto dentro dele
-    
-Test().run() #Para rodar o appcla
+        box = BoxLayout(orientation='vertical') #Chamo a classe boxlayout e instancio ela na variavel box
+                        #Dentro de parenteses posso colocar propriedades para mudar orientacao dos widgets
+        botao = Button(text='Botao 1') #instancio a classe botaa com texto em botao
+        label_tela = Label(text='Texto 1') #Classe para texto
+        box.add_widget(botao) #Adiciono o botao no boxlayout instaciado
+        box.add_widget(label_tela) #Adiciono o texto no outro widget que dividira tela
+        
+        box2 = BoxLayout() 
+        botao2 = Button(text='Botao 2') 
+        label_tela2 = Label(text='Texto 2')
+        box2.add_widget(botao2)
+        box2.add_widget(label_tela2) 
+
+        box.add_widget(box2)#Adiciono um box layout dentro do outro para formar
+        #duas telas na horizontal e duas na vertical dividindo o box em 4 partes
+
+        return box #Construo o box inteiro com tudo dentro
+Test().run() #Para rodar o app
