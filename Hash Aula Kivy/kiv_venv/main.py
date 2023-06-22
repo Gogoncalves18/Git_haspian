06d1e1f12e1b1c624b3eef817b805db7e457d8ff
Gogoncalves18 <gogoncalves@gmail.com>
2023-06-21 21:57:56 -0300

    Aula 3 - acessando funcao e construindo acoes atraves do botao soltar para mexer em variavel em outro widget

--- Hash Aula Kivy/kiv_venv/main.py
+++ Hash Aula Kivy/kiv_venv/main.py
@@ -8,19 +8,23 @@ class Test(App): #Herdo da classe app para minha classe teste do app que farei
     def build(self): #chamo a funcao que controi o app
         box = BoxLayout(orientation='vertical') #Chamo a classe boxlayout e instancio ela na variavel box
                         #Dentro de parenteses posso colocar propriedades para mudar orientacao dos widgets
-        botao = Button(text='Botao 1') #instancio a classe botaa com texto em botao
-        label_tela = Label(text='Texto 1') #Classe para texto
+        botao = Button(text='Botao 1',
+                       font_size=30,
+                       on_release=self.incrementar,
+                       ) 
+                        #instancio a classe botaa com texto em botao
+                        #Pode receber tamanho da fonte
+                        #On_release ou on_press são eventos que quando acontence na classe Button, posso acionar uma funcao ao qual devo chamar como self.nome_da_funcao
+        self.label_tela = Label(text='1') #Classe para texto
+            #Adiciono o self. no label para instanciar ele como uma variavel unica dentro da classe Test, assim posso acessar ele de qualquer lugar dentro da classe
         box.add_widget(botao) #Adiciono o botao no boxlayout instaciado
-        box.add_widget(label_tela) #Adiciono o texto no outro widget que dividira tela
-        
-        box2 = BoxLayout() 
-        botao2 = Button(text='Botao 2') 
-        label_tela2 = Label(text='Texto 2')
-        box2.add_widget(botao2)
-        box2.add_widget(label_tela2) 
-
-        box.add_widget(box2)#Adiciono um box layout dentro do outro para formar
-        #duas telas na horizontal e duas na vertical dividindo o box em 4 partes
-
+        box.add_widget(self.label_tela) #Adiciono o texto no outro widget que dividira tela
+    
         return box #Construo o box inteiro com tudo dentro
+    
+    def incrementar(self, botao): #Aqui devo montar o padrao da funcao e depois chamar a variavel que instanciei a classe Button
+        botao.text='Soltei' #Com a variavel instaciada, posso alterar a propriedade texto dentro dela
+        self.label_tela.text = str(int(self.label_tela.text)+1)
+            #chamo a variavel label atraves do self. da classe e a propriedade dele .text, transformo ele em numero para somar e depois em texto para colocar dentro do label
+
 Test().run() #Para rodar o app
