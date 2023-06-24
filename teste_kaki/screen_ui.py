be3b62a59210620d0e92ca084f3404d430a3ccfd
Gogoncalves18 <gogoncalves@gmail.com>
2023-06-24 13:35:09 -0300

    CONFIGURACAO DE HOT RELOAD PARA KIVY E MDKIVY

---
+++ teste_kaki/screen_ui.py
@@ -0,0 +1,5 @@
+#Neste caso apenas construo uma classe para passar para o kaki e conversar com o ".kv"
+from kivy.uix.boxlayout import BoxLayout
+
+class Tela(BoxLayout): #O nome da classe "Tela" é o nome que devo chamar dentro do arquivo "lay_tela.kv"
+    pass
---
+++ teste_kaki/teste.py
@@ -0,0 +1,7 @@
+import os
+
+#print(os.path.join(os.getcwd(), 'tAqui.txt'))
+
+d = os.path.join(os.getcwd(), 'screen_ui.kv')
+
+print(f'\nCAMINHO: {d}')
