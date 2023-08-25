3f315fb7fc2360d110a5ae1ad3cf71a3ca8d5a51
Gogoncalves18 <gogoncalves@gmail.com>
2023-08-25 00:40:02 -0300

    COntrole dos label no KV atraves de cores, fonte e classes

---
+++ kivy_testes/codeme_com/aula_09/aula_09.py
@@ -0,0 +1,18 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+#Definicao de tamanho de janela
+from kivy.core.window import Window
+
+Builder.load_file('aula_09.kv')
+
+class Box(Widget): 
+    pass      
+
+class Meu_app(App): 
+    def build(self):
+        Window.size = [300,560] #Precisa ser uma lista de valores
+        return Box()
+    
+if __name__ == '__main__':
+    Meu_app().run()
