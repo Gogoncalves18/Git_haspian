5782955518866da09e597c1c9a16f1191a490411
gogoncalves <gogoncalves@gmail.com>
2024-02-28 19:44:26 -0300

    aula 2

---
+++ Uninter/Logica_programacao/aula_02.py
@@ -0,0 +1,13 @@
+print(2+3)
+print(10/3)
+print(10 % 3)  # Resultado e o resto da operação"
+nota = 8.5
+print(nota)
+frase = 'ola seu viado mané'
+print(frase[10])
+s1 = 'estou estudando:'
+s2 = 'logica de programação'
+print(s1 + '>' * 10 + s2)
+print(f'Vc está estudando {s2}')
+nome = input('Qual seu nome completo?')
+print(f'O nome dele é {nome}')
