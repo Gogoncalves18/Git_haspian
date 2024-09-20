12d76cf5a2027edef81c84a5de28170f467be070
gogoncalves <gogoncalves@gmail.com>
2024-09-20 17:46:55 -0300

    AULA 4 - EX 2 - TASK AWAIT ASYNC finalizado

--- Uninter/Fundamentos.Programacao.I/Aula4/Program.cs
+++ Uninter/Fundamentos.Programacao.I/Aula4/Program.cs
@@ -2,7 +2,11 @@
 
 class Program
 {
-    static void Main(string[] args)
+    /* Toda main que usa um metodo de TASK Assincrona, isto e, espera ela responder para seguir
+    e necessario usar o termo "async" "Task" antes do Main, isto por ela inicializa uma Task por
+    natural e so respeitara as demais TASK se elas forem declaradas ao longo do corpo da Main.
+    */
+    static async Task Main(string[] args)
     {
         Console.WriteLine("\nAula Pratica 04 C#");
         Console.WriteLine("\n############################################################");
@@ -17,9 +21,19 @@ class Program
 
         Console.WriteLine("\n_________________________TASKS____________________\n");
 
-        var Ex_Task_02 = new PessoaFinder();
-        Ex_Task_02.ExPessoaAsync();
+        /* Neste caso instancio a class "PessoaFinder()" em uma variavel com o metodo construtor
+        porem ainda preciso chamar o metodo da class que dara inicio as atividades. Esta class 
+        "PessoaFinder()" precisa ter o "async Task" que a define como assincrona e espera uma 
+        resposta das "TASKS" que estao dentro dela. Importante e instanciar em uma variavel do 
+        tipo TASK pois receberei as TASKs proveniente dela, por isto do "Task Ex_Task_02"
+        */
+        Task Ex_Task_02 = new PessoaFinder().ExPessoaAsync();
 
+        // Na variavel ao qual estou recebendo da minha class "PessoaFinder", eu uso o metodo
+        // da TASK para esperar a resposta delas. Importante, dentro da class eu tenho 3 TASKS
+        // independentes, la eu uso um metodo "await Task.WhenAll" para esperar todas terminarem
+        // so assim a minha linha abaixo deixa MAIN prosseguir
+        Ex_Task_02.Wait();
 
         Console.WriteLine("\n############################################################\n");
 
